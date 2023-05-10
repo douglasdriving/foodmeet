a07076150b87a3f595b73d545ab76d53eff8fad6
douglasdriving <douglasdriving@gmail.com>
2023-05-10 10:43:37 -0600

    more progress on the form

--- src/components/dinners/createMeetForm/createMeetForm.js
+++ src/components/dinners/createMeetForm/createMeetForm.js
@@ -11,38 +11,42 @@ export default function CreateMeetForm() {
 
       <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
         <h1>Create Meet</h1>
-        <p>⚠️ Before creating your meet, make sure to reserva a table at the restaurant you want to go to</p>
+        <p>⚠️ Before creating your meet, make sure to reserve a table at the restaurant you want to go to</p>
         {/*Form */}
         <form>
           <label>
-            <p class='inputLabel'>Restaurant Name</p>
-            <input type="text" name="restaurant" />
-          </label>
-
-          {/*booking date, select from calendar*/}
-          <label>
-            <p class='inputLabel'>Date</p>
-            <input type="text" name="date" />
-          </label>
-        
-
-          <label>
-            <p class='inputLabel'>Time</p>
-            <input type="text" name="time" />
+            <p class='inputLabel'>Restaurant</p>
+            <input type="text" name="restaurant" placeholder='Name of the restaurant'/>
           </label>
           <label>
-            <p class='inputLabel'>Seats</p>
-            <input type="text" name="seats" />
+            <p class='inputLabel'>Date & Time</p>
+            <input type="datetime-local" name="datetime" />
           </label>
           <label>
-            <p class='inputLabel'>Notes</p>
-            <input type="text" name="notes" />
+            <p class='inputLabel'>Seats Reserved</p>
+            <select>
+              <option value="">--Please choose an option--</option>
+              {[3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value) =>
+                <option key={value} value={value}>{value}</option>
+              )}
+            </select>
+          </label>
+          <label>
+            <p class='inputLabel'>Reservation Name</p>
+            <input type="text" name="name" placeholder='Name on the reservation'/>
+          </label>
+          <label>
+            <p class='inputLabel'>Invitation</p>
+            <textarea
+              name="Invitation"
+              placeholder={`Write something exciting about the meet`}
+            />
           </label>
           <button type="submit">Create</button>
         </form>
 
       </Modal>
 
-    </div>
+    </div >
   );
 }
