9078d8a560d0652d898c927c5ec9e8359c485474
douglasdriving <douglasdriving@gmail.com>
2023-05-12 10:36:50 -0600

    size fiz
    made sure that the app is adjusted to the visible height of app on the users screen (required for mobile)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
 import Header from './components/header/header';
 import Dinners from './components/dinners/dinners';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // Set the height on load
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+
+    // Listen to resize events and update the height
+    window.addEventListener('resize', () => {
+      document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+    });
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', () => {
+        document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+      });
+    };
+  }, []);
+
   return (
     <div className="App">
       <div className='Cover'>
